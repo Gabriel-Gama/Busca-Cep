5ede386dca94c949b03ed5efc397c81c2a594e31
Gabriel Dias <gabrieldiasgama@hotmail.com>
2022-09-26 14:38:16 -0300

    Api Viacep

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+app.use(express.json());
+
+app.get("/cep/:cep", async (request, response) => {
+    const {cep} = request.params;
+      const result = await axios
+            .get(`https://viacep.com.br/ws/${cep}/json/`)
+            .then((response) => {
+                return response.data
+            }).catch(function(){
+                console.log("Ops! Cep não encontrado!")
+            })
+    return response.json({result});
+})
+
+
+
+app.listen(3333, () => console.log("Server is running!"));
